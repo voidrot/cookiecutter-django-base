6f43bb93526418189b7905bfc914a8c18e8350f0
Buck Brady <22723438+voidrot@users.noreply.github.com>
2025-01-27 00:47:58 -0700

    correct default settings modules name

--- {{ cookiecutter.project_slug }}/config/asgi.py
+++ {{ cookiecutter.project_slug }}/config/asgi.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(BASE_DIR / "{{ cookiecutter.project_slug }}"))
 
 # If DJANGO_SETTINGS_MODULE is unset, default to the local settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
 # This application object is used by any ASGI server configured to use this file.
 django_application = get_asgi_application()
